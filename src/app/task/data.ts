eb75a983f7d023639aaff6b7df161b82b02670d6
Levinsiva <levin.arunachalam@gmail.com>
2021-09-05 09:26:15 +0530

    People Availablity Status Change Task

---
+++ src/app/task/data.ts
@@ -0,0 +1,317 @@
+const data = [{
+        "id": 1,
+        "name": "Kristie Minsall",
+        "phone": "+61415148804",
+        "addon": "AE+C?",
+        "availability": [{
+            "day": "Tue",
+            "date": "29",
+            "month": "Oct",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Wed",
+            "date": "30",
+            "month": "Oct",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Thu",
+            "date": "31",
+            "month": "Oct",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Fri",
+            "date": "1",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Sat",
+            "date": "2",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Sun",
+            "date": "3",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Mon",
+            "date": "4",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Tue",
+            "date": "5",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Wed",
+            "date": "6",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Thu",
+            "date": "7",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Fri",
+            "date": "8",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Sat",
+            "date": "9",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Sun",
+            "date": "10",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Mon",
+            "date": "11",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }]
+    },
+
+    {
+        "id": 2,
+        "name": "Sarah Symons",
+        "phone": "+61402148340",
+        "addon": "AE+C?",
+        "availability": [{
+            "day": "Tue",
+            "date": "29",
+            "month": "Oct",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Wed",
+            "date": "30",
+            "month": "Oct",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Thu",
+            "date": "31",
+            "month": "Oct",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Fri",
+            "date": "1",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Sat",
+            "date": "2",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Sun",
+            "date": "3",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Mon",
+            "date": "4",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Tue",
+            "date": "5",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Wed",
+            "date": "6",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Thu",
+            "date": "7",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Fri",
+            "date": "8",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }, {
+            "day": "Sat",
+            "date": "9",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Sun",
+            "date": "10",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Mon",
+            "date": "11",
+            "month": "Nov",
+            "status": "haventIndicated",
+            "statusKey": "HI"
+
+        }]
+    }, {
+        "id": 3,
+        "name": "Kylie Jolly",
+        "phone": "+61422131209",
+        "addon": "AE+C?",
+        "availability": [{
+            "day": "Tue",
+            "date": "29",
+            "month": "Oct",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Wed",
+            "date": "30",
+            "month": "Oct",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Thu",
+            "date": "31",
+            "month": "Oct",
+            "status": "available",
+            "statusKey": "A"
+
+        }, {
+            "day": "Fri",
+            "date": "1",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Sat",
+            "date": "2",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Sun",
+            "date": "3",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Mon",
+            "date": "4",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Tue",
+            "date": "5",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Wed",
+            "date": "6",
+            "month": "Nov",
+            "status": "available",
+            "statusKey": "A"
+
+        }, {
+            "day": "Thu",
+            "date": "7",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Fri",
+            "date": "8",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }, {
+            "day": "Sat",
+            "date": "9",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Sun",
+            "date": "10",
+            "month": "Nov",
+            "status": "holiday",
+            "statusKey": ""
+
+        }, {
+            "day": "Mon",
+            "date": "11",
+            "month": "Nov",
+            "status": "unavailable",
+            "statusKey": "UA"
+
+        }]
+    }
+]
+export default data;
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import data from './data';
+
+const status = {
+  available:'A',
+  unavailable:'UA',
+  haventIndicated: 'HI'
+}
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+
+export class TaskComponent implements OnInit {
+  showModal: boolean | undefined;
+  title: string | undefined;
+  data:typeof data | undefined;
+  empId:number |undefined;
+  date:string |undefined;
+
+  //Bootstrap Modal Open event
+  show(empId:number,date:string) {
+    this.empId = empId;
+    this.date = date;
+    this.showModal = true; 
+    this.title = "Change Status"; 
+  }
+  update(value: string){
+    this.showModal = false;
+    this.data?.forEach(eachData=>{
+      if(eachData.id === this.empId){
+        eachData.availability.forEach(eachAvailability=>{
+          if(eachAvailability.date===this.date){
+            eachAvailability.status=value;
+            eachAvailability.statusKey=status[value as ('available' | 'unavailable'|'haventIndicated')];
+          }
+        })
+      }
+    })
+  }
+  //Bootstrap Modal Close event
+  hide() {
+    this.showModal = false;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+    this.data=data
+  }
+
+}
